839c2ce26494e87e0670ca3e33d376b3a2445e0b
Isainjimenez <61337276+Isainjimenez@users.noreply.github.com>
2022-07-04 19:50:50 -0500

    Add files via upload

---
+++ Long y/CalInt.py
@@ -0,0 +1,3 @@
+def CalInt(n):
+    return  lambda x: x*n
+result = CalInt(5)
---
+++ Long y/Principal.py
@@ -0,0 +1,8 @@
+import CalLong
+import CalInt
+
+
+
+print(("CalculadoraLong ==> El resultado es: ", CalLong.result(3)).calculate(6,2))
+print(("Calculadoraint ==> El resultado es: ",CalInt.result(2)).calculate(5.5))
+
